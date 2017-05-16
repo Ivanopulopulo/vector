b7526a579598179a7ca0b1210256734a523851b1
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-16 14:32:44 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -63,5 +63,27 @@ SCENARIO("vector pushback", "[init]")
 	REQUIRE(vector[2]==0);
 	REQUIRE(vector[3]==4);
 }
-	
+SCENARIO("delval")
+	vector_t vector(2);
+	vector.push_back(1);
+	vector.push_back(2);
+	vector.push_back(3);
+	REQUIRE(vector.size() == 5);
+	REQUIRE(vector.capacity() == 8);
+	REQUIRE(vector.delval() == 3);
+	REQUIRE(vector.size() == 4);
+	REQUIRE(vector.capacity() == 4);
+	REQUIRE(vector.delval() == 2);
+	REQUIRE(vector.size() == 3);
+	REQUIRE(vector.capacity() == 4);
+	REQUIRE(vector.delval() == 1);
+	REQUIRE(vector.size() == 2);
+	REQUIRE(vector.capacity() == 2);
+	REQUIRE(vector.delval() == 0);
+	REQUIRE(vector.size() == 1);
+	REQUIRE(vector.capacity() == 1);
+	REQUIRE(vector.delval() == 0);
+	REQUIRE(vector.size() == 0);
+	REQUIRE(vector.capacity() == 0);
+
 	

518cfbecae281a1cb7dd3c2e56a40581b1f480b0
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-16 14:25:24 +0400

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -16,5 +16,6 @@ public:
     auto size() const noexcept -> unsigned int;
     auto capacity() const noexcept -> unsigned int;
     auto push_back(int value) -> void;
+    auto delval() -> int;
     auto operator[](unsigned int index) const noexcept -> int;
 };

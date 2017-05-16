53d8fadda62a971252e431b16303fe15b820b401
Ivanopulopulo <vkuvshinov.ru@gmail.com>
2017-05-16 15:59:43 +0400

    Update vector.cpp

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -1,100 +1,93 @@
 #include "vector.hpp"
+
 vector_t::vector_t() noexcept
 {
-  size_ = 0;
-  capacity_=0;
-  ptr_=new int[capacity_];
+	size_ = 0;
+	capacity_ = 0;
+	ptr_ = nullptr;
 }
 vector_t::vector_t(unsigned int size)
 {
-  size_=size;
-  capacity_=size;
-  ptr_=new int[capacity_]{};
+	size_ = size;
+	capacity_ = size;
+	ptr_ = new int[capacity_];
+	for (unsigned int i = 0; i < size_; i++)
+		ptr_[i] = 0;
 }
 vector_t::vector_t(const vector_t& other)
 {
-  size_ = other.size_;
-  capacity_=other.capacity_;
-  ptr_=new int [capacity_];
-  for(unsigned int i = 0; i < size_; i++)
-  {
-    ptr_[i] = other.ptr_[i];
-  }
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+	ptr_ = new int[capacity_];
+	for (unsigned int i = 0; i < size_; i++)
+		ptr_[i] = other.ptr_[i];
 }
 auto vector_t::operator=(const vector_t& other)->vector_t&
 {
-    if(this != &other)
-    {
-      delete[]ptr_;
-      size_=other.size_;
-      capacity_=other.capacity_;
-      ptr_=new int[capacity_];
-      for (unsigned int i = 0; i <size_; i++)
-      {
-        ptr_[i]=other.ptr_[i];
-      }
-    }
-  return *this;
+	if (&other != this) {
+		delete[] ptr_;
+		size_ = other.size_;
+		capacity_ = other.capacity_;
+		ptr_ = new int[capacity_];
+		for (unsigned int i = 0; i < size_; i++)
+			ptr_[i] = other.ptr_[i];
+	}
+	return *this;
 }
 vector_t::~vector_t() noexcept
 {
-  size_=0;
-  capacity_=0;
-  delete[] ptr_;
+	size_ = 0;
+	capacity_ = 0;
+	delete[] ptr_;
 }
 auto vector_t::size() const noexcept -> unsigned int
 {
-  return size_;
+	return size_;
 }
 auto vector_t::capacity() const noexcept -> unsigned int
 {
-  return capacity_;
+	return capacity_;
 }
 auto vector_t::push_back(int value) -> void
 {
-  if (capacity_ == 0)
-  {
-    delete[] ptr_;
-    capacity_=1;
-    ptr_= new int[capacity_];
-  }
-  else if (size_ +1 >= capacity_)
-  {
-    capacity_ += 3;
-      int*ptra_ = new int[capacity_];
-    for(unsigned int i = 0; i< size_; i++)
-    {
-      ptra_[i]=ptr_[i];
-    } 
-    delete[]ptr_;
-    ptr_=ptra_;
-  }
-  ptr_[size_++] = value;
+	if (capacity_ == 0)
+	{
+		capacity_ = 1;
+		ptr_ = new int[capacity_];
+	}
+	if (capacity_ == size_)
+	{
+		capacity_ = capacity_ * 2;
+		int *ptr1 = new int[capacity_];
+		for (unsigned int i = 0; i < size_; i++)
+			ptr1[i] = ptr_[i];
+		delete[] ptr_;
+		ptr_ = ptr1;
+	}
+	ptr_[size_] = value;
+	size_++;
 }
 auto vector_t::delval() -> int
 {
-  if (size_==0)
-    {  
-       system("pause");
-       exit(1);
-    }
-if (capacity_>=2*(size_-1))
-    {   
-        capacity_=capacity_/2;  
-    }
- int *ptr1=new int [capacity_];
-for(unsigned int i = 0; i<size_-1; i++)
-    ptr1[i]=ptr_[i];
- int n= ptr_[size_ -1];
-  delete[] ptr_;
-  size_--;
-  return n;
+	if (size_ == 0)
+	{
+		system("pause");
+		exit(1);
+	}
+	if (capacity_ >= 2 * (size_ - 1))
+	{
+		capacity_ = capacity_ / 2;	
+	}
+	int *ptr1 = new int[capacity_];
+	for (unsigned int i = 0; i < size_ - 1; i++)
+		ptr1[i] = ptr_[i];
+	int n= ptr_[size_ - 1];
+	delete[] ptr_;
+	ptr_ = ptr1;
+	size_--;
+	return n;
 }
- auto vector_t::operator[](unsigned int index) const noexcept -> int
+auto vector_t::operator[](unsigned int index)  const noexcept -> int
 {
-  if(index >= size_)
-    throw "Out of range!!!\n";
-  return ptr_[index];
+	return ptr_[index];
 }
-
-  
